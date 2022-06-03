1f4a1e640050e99ed8891df0f7797abdbc0dd343
Bot473Gaming <90602541+Bot473Gaming@users.noreply.github.com>
2022-06-03 15:11:27 +0700

    Add files via upload

--- 7/assets/js/main.js
+++ 7/assets/js/main.js
@@ -7,15 +7,22 @@ var p1 = document.getElementsByClassName('tank')[0],
     p2 = document.getElementsByClassName('tank')[1],
     pbg1 = document.getElementsByClassName('tank-ctn')[0],
     pbg2 = document.getElementsByClassName('tank-ctn')[1],
-    hp1 = hp2 = 100;
+    hp1 = hp2 = 100,
+    fort = document.getElementsByClassName('fort'),
+    hpf1 = hpf2 = 300;
 var iscl = false,
     isf1 = true,
     isf2 = true;
-pbg1.style.left = '20px';
-pbg1.style.top = '20px';
+pbg1.style.left = '275px';
+pbg1.style.top = '200px';
 
-pbg2.style.left = '500px';
-pbg2.style.top = '500px';
+pbg2.style.left = '275px';
+pbg2.style.top = '400px';
+
+fort[0].style.left = '265px';
+fort[0].style.top = '12px';
+fort[1].style.left = '265px';
+fort[1].style.top = '528px';
 
 var game = true;
 
@@ -48,7 +55,7 @@ function getAndchoose(x, y, h) {
     switch (h) {
         case 0:
             x += 20;
-            y -= 20;
+            y -= 25;
             break;
         case 1:
             x += 65;
@@ -82,35 +89,46 @@ function changePosBullet(x, y, h) {
     return { x: x + 'px', y: y + 'px' };
 }
 
-function getPosForCheck(pl) {
-    let x1 = parseInt(String(pl.style.left).replace('px', '')),
-        y1 = parseInt(String(pl.style.top).replace('px', ''));
-    let x2 = x1 + 50,
-        y2 = y1 + 50;
+function ccc(x, y, h) {
+    switch (h) {
+        case 0:
+            y += 5;
+            break;
+        case 1:
+            x -= 5;
+            break;
+        case 2:
+            y -= 5;
+            break;
+        case 3:
+            x += 5;
+    }
+    return { x: x + 'px', y: y + 'px' };
+}
+
+function getPos(pl) {
+    let x1 = $(pl).offset().left,
+        y1 = $(pl).offset().top;
+    let x2 = x1 + $(pl).width(),
+        y2 = y1 + $(pl).height();
     return { x1: x1, x2: x2, y1: y1, y2: y2 };
 }
 
 function isCollect(x, y) {
-    let pos_1 = getPosForCheck(document.getElementsByClassName('tank-ctn')[0]),
-        pos_2 = getPosForCheck(document.getElementsByClassName('tank-ctn')[1]);
-    // console.log(x, y, pos_1)
-    let hpp1 = document.getElementsByClassName('hp')[0],
-        hpp2 = document.getElementsByClassName('hp')[1]
-    if (x >= pos_1.x1 && x <= pos_1.x2 && y >= pos_1.y1 && y <= pos_1.y2) {
-        hp1 -= 10;
-        hpp1.style.width = hp1 + '%';
-        iscl = true;
+    let pos_1 = getPos(x),
+        pos_2 = getPos(y);
+    let a = pos_1.x2 > pos_2.x1,
+        b = pos_1.x1 < pos_2.x2,
+        c = pos_1.y2 > pos_2.y1,
+        d = pos_1.y1 < pos_2.y2;
+
+    if (a && b && c && d) {
+        return true
     }
-    if (x >= pos_2.x1 && x <= pos_2.x2 && y >= pos_2.y1 && y <= pos_2.y2) {
-        hp2 -= 10;
-        hpp2.style.width = hp2 + '%';
-        iscl = true;
-    }
+    return false;
 }
 
 (function() {
-    // while (true) {
-    // await delay(100);
     setInterval(function() {
         let bullets = document.getElementsByClassName('bullet');
         try {
@@ -121,15 +139,26 @@ function isCollect(x, y) {
                 let h = parseInt(temp.pop());
                 let index = parseInt(temp.pop());
                 let pos = changePosBullet(x, y, h);
-                // console.log(pos, index, h, isoutWall(pos.x) || isoutWall(pos.y))
-                isCollect(x, y);
-                if (isoutWall(pos.x) || isoutWall(pos.y)) {
+                if (isCollect(bullet, document.getElementsByClassName('tank')[0]) || isCollect(bullet, document.getElementsByClassName('tank')[1])) {
+                    // console.log('collection');
+                    if (isCollect(bullet, document.getElementsByClassName('tank')[0])) {
+                        hp1 -= 10;
+                    } else {
+                        hp2 -= 10;
+                    }
                     bullet.remove();
                     continue;
                 }
-                if (iscl) {
-                    // console.log('collection');
-                    iscl = false;
+                if (isoutWall(pos.x) || isoutWall(pos.y) || isCollect(bullet, $('.wl'))) {
+                    bullet.remove();
+                    continue;
+                }
+                if (isCollect(bullet, $('.fort').eq(0)) || isCollect(bullet, $('.fort').eq(1))) {
+                    if (isCollect(bullet, $('.fort').eq(0))) {
+                        hpf1 -= 10;
+                    } else {
+                        hpf2 -= 10;
+                    }
                     bullet.remove();
                     continue;
                 }
@@ -143,7 +172,6 @@ function isCollect(x, y) {
         }
 
     }, 5);
-    // }
 })();
 
 (async function() {
@@ -168,30 +196,42 @@ function isCollect(x, y) {
         hp2 = Math.max(0, hp2);
 
         if (!hp1 || !hp2) {
-            game = false;
-            // console.log('end')
-            document.getElementsByClassName('reset')[0].classList.add('in')
+            console.log('end')
+            let pbg = document.getElementsByClassName('tank-ctn');
+
             if (!hp1) {
-                document.getElementsByClassName('win')[0].innerHTML = 'P2 WIN';
+                pbg[0].style.left = '275px';
+                pbg[0].style.top = '200px';
+                hp1 = 100;
             } else {
-                document.getElementsByClassName('win')[0].innerHTML = 'P1 WIN'
+                pbg[1].style.left = '275px';
+                pbg[1].style.top = '400px';
+                hp2 = 100;
             }
+        }
+        if (!hpf1 || !hpf2) {
+            game = false;
+            document.getElementsByClassName('reset')[0].classList.add('in')
+            document.getElementsByClassName('win')[0].innerHTML = 'P2 WIN';
+            document.getElementsByClassName('win')[0].innerHTML = 'P1 WIN'
             clearInterval(tx);
+
         }
         // console.log(d1, d2)
     }, 50);
 })();
 ////
+console.log(getPos($('.tank').eq(1)))
 var keys = {};
 document.onkeydown = document.onkeyup = (event) => {
     // console.log(event.keyCode)
     // console.log(event.type)
+    if (!game) {
+        return;
+    }
     if (event.type == 'keydown') {
         keys[event.keyCode] = true;
 
-    }
-    if (!game) {
-        return;
     }
     // console.log(keys)
     if (event.type == 'keyup') {
@@ -200,39 +240,41 @@ document.onkeydown = document.onkeyup = (event) => {
     }
 
 };
-var xxx = setInterval(() => {
+var xxx = setInterval(async() => {
+        if (!game) {
+            clearInterval(xxx);
+        }
         var p1 = document.getElementsByClassName('tank')[0],
             p2 = document.getElementsByClassName('tank')[1],
             pbg1 = document.getElementsByClassName('tank-ctn')[0],
             pbg2 = document.getElementsByClassName('tank-ctn')[1],
-            hpp1 = document.getElementsByClassName('hp')[0],
-            hpp2 = document.getElementsByClassName('hp')[1]
+            hpp = document.getElementsByClassName('hp');
 
         if (keys[39]) {
             //right
             h1 = 1;
-            let pos = parseInt(String(pbg1.style.left).replace('px', '')) + 5;
+            let pos = parseInt(String(pbg1.style.left).replace('px', '')) + 2;
             pos = outWall(pos);
             pbg1.style.left = pos + 'px';
         }
         if (keys[37]) {
             //left
             h1 = 3;
-            let pos = parseInt(String(pbg1.style.left).replace('px', '')) - 5;
+            let pos = parseInt(String(pbg1.style.left).replace('px', '')) - 2;
             pos = outWall(pos);
             pbg1.style.left = pos + 'px';
         }
         if (keys[40]) {
             //down
             h1 = 2;
-            let pos = parseInt(String(pbg1.style.top).replace('px', '')) + 5;
+            let pos = parseInt(String(pbg1.style.top).replace('px', '')) + 2;
             pos = outWall(pos);
             pbg1.style.top = pos + 'px';
         }
         if (keys[38]) {
             //up
             h1 = 0;
-            let pos = parseInt(String(pbg1.style.top).replace('px', '')) - 5;
+            let pos = parseInt(String(pbg1.style.top).replace('px', '')) - 2;
             pos = outWall(pos);
             pbg1.style.top = pos + 'px';
         }
@@ -241,7 +283,7 @@ var xxx = setInterval(() => {
         if (keys[68]) {
             //right
             h2 = 1;
-            let pos = parseInt(String(pbg2.style.left).replace('px', '')) + 5;
+            let pos = parseInt(String(pbg2.style.left).replace('px', '')) + 2;
             pos = outWall(pos);
             pbg2.style.left = pos + 'px';
         }
@@ -249,21 +291,21 @@ var xxx = setInterval(() => {
             //left
 
             h2 = 3;
-            let pos = parseInt(String(pbg2.style.left).replace('px', '')) - 5;
+            let pos = parseInt(String(pbg2.style.left).replace('px', '')) - 2;
             pos = outWall(pos);
             pbg2.style.left = pos + 'px';
         }
         if (keys[83]) {
             //down
             h2 = 2;
-            let pos = parseInt(String(pbg2.style.top).replace('px', '')) + 5;
+            let pos = parseInt(String(pbg2.style.top).replace('px', '')) + 2;
             pos = outWall(pos);
             pbg2.style.top = pos + 'px';
         }
         if (keys[87]) {
             //up
             h2 = 0;
-            let pos = parseInt(String(pbg2.style.top).replace('px', '')) - 5;
+            let pos = parseInt(String(pbg2.style.top).replace('px', '')) - 2;
             pos = outWall(pos);
             pbg2.style.top = pos + 'px';
         }
@@ -287,12 +329,30 @@ var xxx = setInterval(() => {
             bl[bl.length - 1].style.left = pos.x;
             bl[bl.length - 1].style.top = pos.y;
             bl[bl.length - 1].style.transform = `rotate(${rota[h1]}deg)`;
+            console.log(hpf1, hpf2)
         }
-        hpp1.style.width = hp1 + '%';
-        hpp2.style.width = hp2 + '%';
+        if (isCollect(pbg1, $('.wl')) || isCollect(pbg1, $('.fort').eq(0)) || isCollect(pbg1, $('.fort').eq(1))) {
+            let x = parseInt(String(pbg1.style.left).replace('px', ''));
+            let y = parseInt(String(pbg1.style.top).replace('px', ''));
+            let pos = ccc(x, y, h1);
+            pbg1.style.left = pos.x;
+            pbg1.style.top = pos.y;
+
+        }
+        if (isCollect(pbg2, $('.wl')) || isCollect(pbg2, $('.fort').eq(0)) || isCollect(pbg2, $('.fort').eq(1))) {
+            let x = parseInt(String(pbg2.style.left).replace('px', ''));
+            let y = parseInt(String(pbg2.style.top).replace('px', ''));
+            let pos = ccc(x, y, h2);
+            pbg2.style.left = pos.x;
+            pbg2.style.top = pos.y;
+        }
+        hpp[2].style.width = hpf1 / 3 + '%';
+        hpp[3].style.width = hpf2 / 3 + '%';
+        hpp[0].style.width = hp1 + '%';
+        hpp[1].style.width = hp2 + '%';
         p1.style.transform = `rotate(${rota[h1]}deg)`
         p2.style.transform = `rotate(${rota[h2]}deg)`
-    }, 10)
+    }, 1)
     // document.addEventListener('keyup', (event) => {
     //     console.log(keys)
 
